8a5c334e46ae9f6da246c5b9faca15a3956c7515
Ronan O'Brien <18034798+rtobrien@users.noreply.github.com>
2021-08-02 17:05:51 +1000

    comments and clarity

--- fileHashComparison.ps1
+++ fileHashComparison.ps1
@@ -1,10 +1,17 @@
-Get-ChildItem | `
+Get-ChildItem -filter "*hashes.csv" | `
 ForEach-Object {
-    $hash = (Get-FileHash -Algorithm SHA256 $_.FullName).Hash
-    $name = $_.BaseName + $_.Extension
-    Write-Output $name",",$hash
-    $obj = New-Object PSObject -Property @{ Name = $name; Hash = $hash }
-    $obj | Select-Object Name,Hash | Export-Csv sha256_hashes.csv -Append
+
+    $hash = (Get-FileHash -Algorithm SHA256 $_.FullName).Hash #drop the extra stuff
+    $name = $_.Name
+    
+    $filename = "sha256_hashes.csv"
+    #If the file already exists, we're checking the NEW hashes, so create another file.
+    if (test-path -Path $filename -PathType Leaf) {
+        $filename = "new_sha256_hashes.csv"
+    }
+    $obj = New-Object PSObject -Property @{ Name = $name; Hash = $hash } #Create an obj to avoid superfluous detail
+    Write-Output $obj
+    $obj | Select-Object Name,Hash | Export-Csv $filename -Append
 }
 
 Compare-Object -ReferenceObject $old -DifferenceObject $new -Property name,hash -IncludeEqual -PassThru | `
