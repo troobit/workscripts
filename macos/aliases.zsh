b021cc35eab7420f824ba2e9e7db9ea27577830e
Ronan O'Brien <r@rtob.dev>
2025-09-29 11:48:03 +1000

    Updates

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,28 +1,20 @@
-# You can put files here to add functionality separated per file, which
-# will be ignored by git.
-# Files on the custom/ directory will be automatically loaded by the init
-# script, in alphabetical order.
-
+alias ll='ls -laH'
 alias tf='terraform'
-alias vim='nvim'
 alias tfaaa='terraform apply --auto-approve'
 alias tfddd='terraform destroy --auto-approve'
 alias gl='git log --oneline --graph'
 alias ggc='vim ~/.gitconfig'
 alias c='clear'
 alias py='python3'
-alias brup="brew update; brew upgrade; brew upgrade --cask --greedy; brew cleanup -s"
+alias brup="brew update; brew upgrade; brew upgrade --cask --greedy; brew uninstall microsoft-auto-update; brew cleanup -s"
 alias guck='git remote get-url origin | read origin && git rev-parse --show-toplevel | read repo && cd "$repo/.." && rm -rf "$repo" && git clone $origin && cd $repo && repo="" && origin=""'
 alias gitprune='git remote prune origin && git branch -vv | grep '\''origin/.*: gone]'\'' | awk '\''{print $1}'\'' | xargs git branch -D'
 alias ssmsesh='aws ssm start-session --region ap-southeast-2 --target'
 alias zshconfig="code ~/.zshrc"
+alias dockernuke='docker stop $(docker ps -aq); docker rm $(docker ps -aq); docker rmi $(docker images -q); docker-buildx prune -af'
+alias dockerclear='docker stop $(docker ps -aq); docker rm $(docker ps -aq); docker rmi $(docker images -q)'
 alias ohmyzsh="code ~/.oh-my-zsh"
-alias c='clear'
-
-export LSCOLORS='ExGxDxDxCxDxDxFxFxexEx'
-#Plugin Options
-# autosuggestions
-export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=500
-# export ARCHFLAGS="-arch x86_64"
-export AWS_PAGER=""
+alias removetheme="cp ~/.zshrc ~/.zshrc.bak; sed -i '' 's/ \"$RANDOM_THEME\"//g' ~/.zshrc; source ~/.zshrc"
+alias gc='gcloud'
+alias cat='bat'
+alias chrdebug="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222"
---
+++ macos/path.zsh
@@ -0,0 +1,13 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# pnpm
+export PNPM_HOME="/Users/ronan/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+# put homebrew at the front so I use those versions
+export PATH=/opt/homebrew/sbin:/opt/homebrew/bin:$PATH
